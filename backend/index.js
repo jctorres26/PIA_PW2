958e6f08bd68aed7297ce0347be0e70babc8dafd
Jose Carlos Torres <jctorresc2606@gmail.com>
2022-09-20 08:21:09 -0500

    deletions

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,9 @@ const respuesta_router = require('./src/routes/respuestaRouter');
 const categoria_router = require('./src/routes/categoriaRouter');
 const like_router = require('./src/routes/likeRouter');
 const dislike_router = require('./src/routes/dislikeRouter');
-const favorito_router = require('./src/routes/favoritoRouter');
 const preguntas_mas_likes_router = require('./src/routes/preguntas_mas_likesRouter');
 const preguntas_mas_dislikes_router = require('./src/routes/preguntas_mas_dislikesRouter');
 const categorias_mas_preguntas_router = require('./src/routes/categorias_mas_preguntasRouter');
-const mes_mas_registros_router = require('./src/routes/mes_mas_registrosRouter');
-const mesmaspreguntas_router = require('./src/routes/mes_mas_preguntasRouter');
 app.use(bodyParser.json());
 //preguntas_mas_likes categorias_mas_preguntas mes_mas_registros mes_mas_preguntas
 app.get('/', (req, res) => {
@@ -31,11 +28,8 @@ app.use('/api',respuesta_router);
 app.use('/api',categoria_router);
 app.use('/api',like_router);
 app.use('/api',dislike_router);
-app.use('/api',favorito_router);
 app.use('/api',preguntas_mas_likes_router);
 app.use('/api',categorias_mas_preguntas_router);
-app.use('/api',mes_mas_registros_router);
-app.use('/api',mesmaspreguntas_router);
 app.use('/api',preguntas_mas_dislikes_router);
 
 app.listen(port, () => {
